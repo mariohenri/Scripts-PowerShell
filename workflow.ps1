ed8899aad1f9a35d4540ef34a56e45d1207e1e2d
Mario Henrique Ferraz <mariohenri@gmail.com>
2018-10-25 14:39:21 -0300

    Add files via upload

---
+++ workflow.ps1
@@ -0,0 +1,14 @@
+﻿function iniciar-editores
+{
+start-process Notepad
+sleep 10 
+Start-process Notepad++
+}
+workflow start-editores
+{
+parallel {
+start-process Notepad
+sleep 5
+Start-process Notepad++
+}
+}
---
+++ workflow2.ps1
@@ -0,0 +1,12 @@
+﻿Workflow workflowDemorado
+{
+while(1)
+{
+(get-date).ToString() + "Script demoradinho"
+start-sleep -Seconds 2
+}
+}
+$wfjob = workflowDemorado -AsJob
+$wfjob
+Receive-Job $wfjob
+Suspend-Job $wfjob
